3df3b7d8bf3c9aec6d0e6790b78e096e72c75326
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-06-17 13:52:20 +0200

    comp header + logo

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
     <!-- header -->
       <!-- logo -->
       <!-- nav-link -->
+    <MyHeader/>
+  
+
+
 
     <!-- main -->
 
@@ -33,23 +37,23 @@
 </template>
 
 <script>
-
+import MyHeader from './components/MyHeader.vue'
 
 export default {
   name: 'App',
   components: {
-
+    MyHeader
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Helvetica, Arial, sans-serif;
 }
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,53 @@
+<template>
+    <header>
+        <img :src="logo" :alt="myName">
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: "MyHeader",
+    data() {
+        return {
+            myName: "Avada Law",
+            logo: require('../assets/images/avada-law-logo.png'),
+            menu: [
+                {
+                    title: "Home"
+                },
+                {
+                    title: "Who We Are"
+                },
+                {
+                    title: "What We Do"
+                },
+                {
+                    title: "Where We Work"
+                },
+                {
+                    title: "Careers"
+                },
+                {
+                    title: "News"
+                },
+                {
+                    title: "Contact"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    header {
+        color: #fff;
+        width: 100%;
+        height: 100px;
+        background-color: #000;
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
+</style>
