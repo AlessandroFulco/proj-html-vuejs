ea1d8ae04a582b0e3ea0e907220942be5b3c5854
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-06-17 16:48:36 +0200

    varie sez main

--- src/App.vue
+++ src/App.vue
@@ -29,9 +29,9 @@
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eius animi ea repudiandae expedita rerum. Lorem ipsum dolor sit amet.
           </p>
-          <div class="btn">
+          <button class="btn">
             LEARN MORE
-          </div>
+          </button>
         </section>
 
         <section>
@@ -41,9 +41,9 @@
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eius animi ea repudiandae expedita rerum. Lorem ipsum dolor sit amet.
           </p>
-          <div class="btn">
+          <button class="btn">
             LEARN MORE
-          </div>
+          </button>
         </section>
 
         <section>
@@ -53,9 +53,9 @@
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eius animi ea repudiandae expedita rerum. Lorem ipsum dolor sit amet.
           </p>
-          <div class="btn">
+          <button class="btn">
             LEARN MORE
-          </div>
+          </button>
         </section>
       </div>
 
@@ -105,26 +105,175 @@
 
         </section>
       </div>
+        <!-- /sez -->
 
 
-
-    </main>
-
-      <!-- /sez -->
-
-      <!-- ask -->
+        <!-- ask -->
+      <div class="banner">
+        <div>
+          <h2>
+            QUESTION ABOUT A JUDICAL ISSUE? ASK OUR EXPERTS.
+          </h2>
+          <button class="btn">
+            ASK AN EXPERT
+          </button>
+        </div>
+      </div>
 
       <!-- sez -->
+      <div id="myClass">
+        <h1>
+          GLOBAL CHALLENGE. WORLD CLASS RESPONSE
+        </h1>
+
+        <hr>
         <!-- comp. x18 -->
+        <div class="list-card">
+          <!-- list 1 -->
+          <ul>
+            <li>
+              <img src="./assets/images/column1-image1.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column1-image2.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column1-image3.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column1-image4.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column1-image5.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column1-image6.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+          </ul>
+
+          <!-- list 2 -->
+          <ul>
+            <li>
+              <img src="./assets/images/column2-image1.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column2-image2.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column2-image3.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column2-image4.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column2-image5.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column2-image6.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+          </ul>
+          <!-- list 3 -->
+          <ul>
+            <li>
+              <img src="./assets/images/column3-image1.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column3-image2.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column3-image3.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column3-image4.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column3-image5.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+            <li>
+              <img src="./assets/images/column3-image6.jpg" alt="Debora Brown">
+              <span>Debora Brown</span>
+            </li>
+          </ul>
+        </div>
+      </div>
       <!-- /sez -->
 
       <!-- cit. -->
+      <div class="cit">
+        <img src="./assets/images/envato-logo.png" alt="Evanto Logo">
+        <p>
+          "We have worked with Avada Law for the past 15 years. As we have grown and evolved, what I have valued most about
+           <br> 
+           our relationship is that Avada Law know us and our business"
+        </p>
+        <h3>
+          Nigel Rawlinson &middot; Senior Partner
+        </h3>
+      </div>
 
       <!-- sez -->
+      <div class="info special">
         <!-- comp. x3 -->
-      <!-- /sez -->
+        <section>
+          <img src="./assets/images/law_7.jpg" alt="Internation">
+          <h3>
+            INTERNATIONAL
+          </h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius ipsam dolore hic?
+          </p>
+        </section>
 
+        <section>
+          <img src="./assets/images/law_7.jpg" alt="Internation">
+          <h3>
+            PARTNERSHIPS
+          </h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius ipsam dolore hic?
+          </p>
+        </section>
+
+        <section>
+          <img src="./assets/images/law_7.jpg" alt="Internation">
+          <h3>
+            EXCELLENCE
+          </h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius ipsam dolore hic?
+          </p>
+        </section>
+
+      </div>
+      <!-- /sez -->
+        
       <!-- Enquiry -->
+      
+
+    </main>
+
+
+
+
+
+
 
     <!-- footer -->
     
@@ -145,6 +294,7 @@ export default {
 
 <style lang="scss">
 $bg-sez: #f5f5f5;
+$bg-banner: #54a096;
 * {
   margin: 0;
   padding: 0;
@@ -180,9 +330,10 @@ $bg-sez: #f5f5f5;
       background-color: $bg-sez;
       display: flex;
       justify-content: center;
-      padding: 70px;
+      padding: 70px 0;
 
       section {
+        border-top: 3px solid $bg-banner;
         flex-basis: calc(70% / 3);  
         padding: 10px;
         background-color: #fff;
@@ -193,9 +344,102 @@ $bg-sez: #f5f5f5;
       background-color: #fff;
       display: flex;
       justify-content: center;
-      padding: 70px;
+      padding: 70px 0;
       section {
         flex-basis: calc(70% / 4);
+      }
+    }
+
+    .banner {
+      background-color: $bg-banner;
+      color: #fff;
+      div {
+        width: 70%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        padding: 40px;
+      }
+    }
+    #myClass {
+      text-align: center;
+      padding: 100px 0;
+      
+      hr {
+        width: 200px;
+        margin: 50px auto;
+        color: $bg-sez
+      }
+      .list-card {
+        display: flex;
+        justify-content: center;
+
+        ul {
+          margin: 0 15px;
+          list-style-type: none;
+
+          li {
+            padding: 20px;
+            margin-bottom: 10px;
+            background-color: $bg-sez;
+            img {
+              height: 60px;
+              width: auto;
+              vertical-align: middle;
+              margin-right: 10px;
+            }
+            span {
+              padding-right: 100px;
+            }
+          }
+        }
+      }
+    }
+    .cit {
+      width: 100%;
+      height: 400px;
+      background-image: url('./assets/images/showeare-parallax.jpg');
+      background-position: center -380px;
+      background-repeat: no-repeat;
+      background-size: cover;
+
+      color: #fff;
+      font-style: italic;
+      text-align: center;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 20px;
+
+      p {
+        line-height: 30px;
+        letter-spacing: 2px;
+      }
+    }
+    .info.special {
+      
+      background-color: $bg-sez;
+      display: flex;
+      justify-content: center;
+      padding: 70px 0;
+      section {
+        border-top: 3px solid $bg-banner;
+        background-color: #fff;
+        padding: 40px;
+        flex-basis: calc(70% / 3);
+
+        img {
+          max-width: 100%;
+        }
+
+        h3 {
+          margin: 20px 0;
+        }
+        p {
+          color: #666;
+          font-size: 14px;
+        }
       }
     }
   }
